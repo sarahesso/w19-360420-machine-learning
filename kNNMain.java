17620973fc294ce0eee127608d2f71de42368c81
Sara Esongola Biango <sara.esongolabiango@dawsoncollege.qc.ca>
2019-05-14 19:36:55 -0400

    finishedMain

--- kNNMain.java
+++ kNNMain.java
@@ -10,6 +10,9 @@ public class kNNMain{
 
     // TASK 1: Use command line arguments to point DataSet.readDataSet method to
     // the desired file. Choose a given DataPoint, and print its features and label
+	double[] accuracy = new double[1000];
+	
+	 for(int k=0; k<1000 ; k++) {	
 	String theData = args[0];
 	List<DataPoint> breastCancer = DataSet.readDataSet(theData);
 	
@@ -17,12 +20,13 @@ public class kNNMain{
 	double [] values = breastCancerPoint.getX();
 	String printableX = Arrays.toString(values);
 
-	System.out.println(printableX); 
-
+	//System.out.println(printableX); 
+	 
+	 
 
     //TASK 2:Use the DataSet class to split the fullDataSet into Training and Held Out Test Dataset
-	List<DataPoint> bCancerTest = DataSet.getTestSet(breastCancer,0.2);
-	List<DataPoint> bCancerTraining = DataSet.getTrainingSet(breastCancer,0.8);
+	List<DataPoint> bCancerTest = DataSet.getTestSet(breastCancer,0.3);
+	List<DataPoint> bCancerTraining = DataSet.getTrainingSet(breastCancer,0.7);
 	
 
     // TASK 3: Use the DataSet class methods to plot the 2D data (binary and multi-class) DON'T DO IT
@@ -38,16 +42,40 @@ public class kNNMain{
     // and make a print a predicted target label
 		KNNClassifier myObject = new KNNClassifier(5);
 		DataPoint [] kNeighbor = myObject.getNearestNeighbors(breastCancer,breastCancerPoint);
-		System.out.println(kNeighbor);
+		//System.out.println(kNeighbor);
 
 
     // TASK 6: loop over the datapoints in the held out test set, and make predictions for Each
     // point based on nearest neighbors in training set. Calculate accuracy of model.
-		for (int i=0; i<kNeighbor.length; i++) {
-			DataPoint myThing = kNeighbor[i]; 
-			System.out.println (myThing.label); 
-		}
-
+		
+	int Correct = 0;
+	 
+     for (int i=0; i<bCancerTest.size(); i++) {
+			 DataPoint myThing = bCancerTest.get(i); 
+			String prediction = myObject.predict(bCancerTraining, myThing);
+			
+			if(myThing.getLabel().equals (prediction))
+			{
+				Correct ++;
+			}
+      }
+	  
+	  double percentage = 100* ((double)Correct / (double)bCancerTest.size());
+	  System.out.println(percentage);
+	  accuracy[k] = percentage;
+	  double mean = (double) Correct / (double) k;
+	  System.out.println("the mean is: " + mean);
+	  double sum =0.;
+	  for( int j=0 ; j<1000 ; j++)
+	  {
+		 sum = Math.pow(accuracy[j]-mean,2);
+	  }
+	  double standardDeviation= Math.sqrt(sum / k);
+	  System.out.println("the standard deviation is: " + standardDeviation);
+		  
+			
+		 
   }
 
+}
 }
